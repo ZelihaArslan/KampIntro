7db53457f5cd59fb707e4e22afa7052dbf8edf57
Zeluha <Zeluha@DESKTOP-EAU7LCO>
2021-01-24 18:35:20 +0300

    Camping

---
+++ Metotlar/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+//Metotlar: tekrar tekrar kullanılabilirliği sağlayan kod bloklarıdır.
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1,urun2 };
+
+            //urun sayısı kadar döner.
+            //Urun : veri tipi
+            foreach (Urun urun in urunler)  //urunler diye bir dizim var her bir elemanını tek tek gezsin ekrana tek tek bassın
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("-------------------");
+            }
+
+            Console.WriteLine("------------------Metotlar--------------");
+            //instance -örnek
+            //encapsulation
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Kiraz", "Kırmızı ", 25,78);
+            sepetManager.Ekle2("Elma", "Yeşil Elma ", 49,88);
+            sepetManager.Ekle2("Erik", "Bol ekşi ", 67,35);
+
+        }
+    }
+}
+//Best Practise: en iyi uygulama teknikleri 
---
+++ Metotlar/Urun.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//bir alışveriş sayfasındaki sepet kısmındaki işlemler ürün ile ilgili işlrmlrt için class açarız
+namespace Metotlar  
+{
+    class Urun //birden fazla eleman tutabilirsin
+    {
+        //Property: Özellik
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public double Fiyati { get; set; }
+        public string Aciklama { get; set; }
+
+
+        public int StokAdedi { get; set; }
+
+    }
+}
